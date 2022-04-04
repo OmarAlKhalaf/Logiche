31e803fa84c74e91e9af25ea334833f86d0f14b2
OmarAlKhalaf <99047548@mydavinci.nl>
2022-04-04 14:25:46 +0200

    (started with gui)

---
+++ Start_gui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+window = tk.Tk()
+window.configure(bg='red')
+window.geometry('200x200') 
+
+button = tk.Button(text = 'Hit mee', bg='white', fg='black')
+button.pack(pady = 20, padx = 20)
+# Code here onder
+
+
+
+# Your code end here
+window.mainloop()
